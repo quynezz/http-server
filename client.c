f7a0c40491e3883561c8eea0e1141ec92a82a2aa
quynezz <laungocquyen098>
2025-08-25 22:34:25 +0700

    finish testing

--- client.c
+++ client.c
@@ -6,30 +6,33 @@
 #include <sys/types.h>
 #include <unistd.h>
 
+/* is client socket code for testing the http_server.c */
+#define BREAK printf("\n-------------------------------------\n");
+
 int client(int client_socket) {
-    ssize_t n = 0;
+    ssize_t valread = 0;
     char buf[1024];
-    char response[] =
-        "HTTP/1.1 200 OK\r\n"
-        "Content-Length: 13\r\n"
-        "Content-Type: text/plain\r\n"
-        "\r\n"
-        "This is 3x3 👻";
+    char response[] = "HTTP/1.1 200 OK\r\n"
+                      "Content-Length: 13\r\n"
+                      "Content-Type: text/plain\r\n"
+                      "\r\n"
+                      "This is 3x3 👻";
     for (;;) {
-        printf("\n-------------------------------------\n");
+        BREAK;
         memset(buf, 0, sizeof(buf));
-        n = read(client_socket, buf, sizeof(buf) - 1);
-        if (n < 0) {
+        valread = read(client_socket, buf, sizeof(buf) - 1);
+        if (valread < 0) {
             perror("read(client_socket)");
             return 1;
         }
-        if (n == 0) {
+        if (valread == 0) {
             printf("Conenction closed by peer\n");
             break;
         }
         printf("Request:\n%s", buf);
         (void)write(client_socket, response, sizeof(response) - 1);
     }
-    printf("-------------------------------------\n");
+    BREAK;
+
     return 0;
 }
